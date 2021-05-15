5c072566c71eac0b84afa09274c29039903db694
Roy Ash <roy.ash456@gmail.com>
2021-05-15 13:32:28 +0300

    little improvements
    Signed-off-by: Roy Ash <roy.ash456@gmail.com>

--- src/main/java/il/co/RedAlert.java
+++ src/main/java/il/co/RedAlert.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -17,31 +18,35 @@ public class RedAlert
 	private static final ObjectMapper MAPPER = new ObjectMapper();
 	private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
 
-	public static void main(String... args) throws IOException
+	public static void main(String... args) throws MalformedURLException
 	{
 		final URL url = new URL("https://www.oref.org.il/WarningMessages/alert/alerts.json");
 		long currLastModified = 0;
 		System.out.println("Starting Red Alert listener...");
 		while (true)
 		{
-			if (url.openConnection() instanceof HttpURLConnection httpURLConnection)
+			try
 			{
-				httpURLConnection.setRequestProperty("Referer", "https://www.oref.org.il/12481-he/Pakar.aspx");
-				httpURLConnection.setRequestProperty("X-Requested-With", "XMLHttpRequest");
-				httpURLConnection.setUseCaches(false);
+				if (url.openConnection() instanceof HttpURLConnection httpURLConnection)
+				{
+					httpURLConnection.setRequestProperty("Referer", "https://www.oref.org.il/12481-he/Pakar.aspx");
+					httpURLConnection.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+					httpURLConnection.setUseCaches(false);
 
-				if (httpURLConnection.getContentLength() > 0 && httpURLConnection.getLastModified() > currLastModified)
-					try (InputStream inputStream = httpURLConnection.getInputStream())
-					{
-						System.out.println(new StringBuilder("Content Length: ").append(httpURLConnection.getContentLength()).append(" bytes").append(System.lineSeparator())
-								.append("Last Modified Date: ").append(SIMPLE_DATE_FORMAT.parse(httpURLConnection.getHeaderField("last-modified"))).append(System.lineSeparator())
-								.append("Current Date: ").append(new Date()).append(System.lineSeparator())
-								.append("Response: ").append(MAPPER.readValue(inputStream, RedAlertResponse.class).data));
-						currLastModified = httpURLConnection.getLastModified();
-					} catch (ParseException e)
-					{
-						e.printStackTrace();
-					}
+					Date lastModified;
+					if (httpURLConnection.getContentLength() > 0 && (lastModified = SIMPLE_DATE_FORMAT.parse(httpURLConnection.getHeaderField("last-modified"))).getTime() > currLastModified)
+						try (InputStream inputStream = httpURLConnection.getInputStream())
+						{
+							currLastModified = httpURLConnection.getLastModified();
+							System.out.println(new StringBuilder("Content Length: ").append(httpURLConnection.getContentLength()).append(" bytes").append(System.lineSeparator())
+									.append("Last Modified Date: ").append(lastModified).append(System.lineSeparator())
+									.append("Current Date: ").append(new Date()).append(System.lineSeparator())
+									.append("Response: ").append(MAPPER.readValue(inputStream, RedAlertResponse.class).data));
+						}
+				}
+			} catch (IOException | ParseException e)
+			{
+				e.printStackTrace();
 			}
 		}
 	}
