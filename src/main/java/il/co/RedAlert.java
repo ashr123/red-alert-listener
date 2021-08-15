bf96f1d6bbd1728aca940e35a8420831581b4929
Roy Ash <roy.ash456@gmail.com>
2021-08-16 02:43:38 +0300

    added help display
    Signed-off-by: Roy Ash <roy.ash456@gmail.com>

--- src/main/java/il/co/RedAlert.java
+++ src/main/java/il/co/RedAlert.java
@@ -17,32 +17,46 @@ import java.util.stream.Collectors;
 
 public class RedAlert
 {
-	private static final Settings DEFAULT_SETTINGS;
+	private static final Settings DEFAULT_SETTINGS = new Settings(
+			false,
+			false,
+			true,
+			false,
+			5000,
+			10000,
+			15,
+			Language.HE,
+			Collections.emptySet()
+	);
+	private static String settingsPath = "red-alert-settings.json";
 	private static Settings settings;
-	private static long settingsLastModified;
-	private static Set<String> districtsNotFound;
+	private static long settingsLastModified = 1;
+	private static Set<String> districtsNotFound = Collections.emptySet();
 	private static HttpURLConnection httpURLConnectionField;
 	private static boolean isContinue = true;
 
-	static
-	{
-		System.err.println("Preparing Red Alert Listener...");
-		DEFAULT_SETTINGS = new Settings(
-				false,
-				false,
-				true,
-				false,
-				5000,
-				10000,
-				15,
-				Language.HE,
-				Collections.emptySet()
-		);
-		districtsNotFound = Collections.emptySet();
-	}
-
 	public static void main(String... args)
 	{
+		if (args.length > 0)
+			switch (args[0])
+			{
+				case "--help" -> {
+					System.out.printf("""
+							Red Alert Listener v%s
+							Options:
+								--help: displays this help text and exits.
+								--settings-file-path <path/to/settings/file.json>: provide a path for a valid settings file, default path is "./red-alert-settings.json" (i.e. from current working directory).%n""", RedAlert.class.getPackage().getImplementationVersion());
+					return;
+				}
+				case "--settings-file-path" -> {
+					if (args.length > 1)
+						settingsPath = args[1];
+					else
+						throw new IllegalStateException("Flag \"--settings-file-path\" must be followed by path to a legal settings file!");
+				}
+				default -> throw new IllegalStateException("Unexpected value: " + args[0]);
+			}
+		System.err.println("Preparing Red Alert Listener v" + RedAlert.class.getPackage().getImplementationVersion() + "...");
 		try (Clip clip = AudioSystem.getClip();
 		     AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(Objects.requireNonNull(RedAlert.class.getResourceAsStream("/alarmSound.wav")))))
 		{
@@ -50,7 +64,7 @@ public class RedAlert
 			new Thread(() ->
 			{
 				final Scanner scanner = new Scanner(System.in);
-				System.err.println("Enter \"t\" for sound test, \"c\" for clearing the screen or \"q\" to quit.");
+				printHelpMsg();
 				while (true)
 					switch (scanner.nextLine())
 					{
@@ -65,12 +79,13 @@ public class RedAlert
 							clip.start();
 						}
 						case "c" -> System.err.println("\033[H\033[2JListening...");
+						case "h" -> printHelpMsg();
 					}
 			}).start();
 			final URL url = new URL("https://www.oref.org.il/WarningMessages/alert/alerts.json");
 			final ObjectMapper objectMapper = new ObjectMapper();
 			final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
-			final File settingsFile = new File("red-alert-settings.json");
+			final File settingsFile = new File(settingsPath);
 			final Districts districts = objectMapper.readValue(RedAlert.class.getResourceAsStream("/districts.json"), Districts.class);
 			loadSettings(objectMapper, districts, settingsFile);
 			Set<String> prevData = Collections.emptySet();
@@ -148,7 +163,7 @@ public class RedAlert
 					if (e instanceof IOException)
 						sleep();
 				}
-		} catch (UnsupportedAudioFileException | LineUnavailableException | IOException e)
+		} catch (UnsupportedAudioFileException | LineUnavailableException | IOException | NullPointerException e)
 		{
 			System.err.println("Fatal error at " + new Date() + ": " + e + System.lineSeparator() +
 					"Closing connection end exiting...");
@@ -158,6 +173,11 @@ public class RedAlert
 		}
 		if (httpURLConnectionField != null)
 			httpURLConnectionField.disconnect();
+	}
+
+	private static void printHelpMsg()
+	{
+		System.err.println("Enter \"t\" for sound test, \"c\" for clearing the screen, \"q\" to quit or \"h\" for displaying this help massage.");
 	}
 
 	private static void printDistrictsNotFoundWarning()
@@ -182,16 +202,16 @@ public class RedAlert
 		final long settingsLastModifiedTemp = settingsFile.lastModified();
 		if (settingsLastModifiedTemp > settingsLastModified)
 		{
-			System.err.println("Info: (re)loading settings from file \"red-alert-settings.json\".");
+			System.err.println("Info: (re)loading settings from file \"" + settingsPath + "\".");
 			settings = objectMapper.readValue(settingsFile, Settings.class);
 			settingsLastModified = settingsLastModifiedTemp;
 			districtsNotFound = settings.districtsOfInterest().parallelStream()
 					.filter(Predicate.not(new HashSet<>(districts.getLanguage().values())::contains))
 					.collect(Collectors.toSet());
 			printDistrictsNotFoundWarning();
-		} else if (settingsLastModifiedTemp == 0)
+		} else if (settingsLastModifiedTemp == 0 && settingsLastModified != 0)
 		{
-			System.err.println("Couldn't find \"red-alert-settings.json\", using default settings");
+			System.err.println("Warning: couldn't find \"" + settingsPath + "\", using default settings");
 			settings = DEFAULT_SETTINGS;
 			settingsLastModified = 0;
 			districtsNotFound = Collections.emptySet();
